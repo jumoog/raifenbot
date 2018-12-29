6640a72176b75b74db9da2454a35686f0d8358ed
Kilian von Pflugk <github@jumoog.io>
2018-12-29 02:04:01 +0100

    date format add days

--- index.js
+++ index.js
@@ -329,15 +329,16 @@ process.on('SIGINT', () => {
     process.exit(0);
 });
 
-function format(seconds) {
-    function pad(s) {
-        return (s < 10 ? '0' : '') + s;
-    }
-    var hours = Math.floor(seconds / (60 * 60));
-    var minutes = Math.floor(seconds % (60 * 60) / 60);
-    var seconds = Math.floor(seconds % 60);
-
-    return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+function format(time) {
+  let days = Math.floor(time % 31536000 / 86400)
+  let hours = Math.floor(time % 31536000 % 86400 / 3600)
+  let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
+  let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
+  days = days > 9 ? days : '0' + days
+  hours = hours > 9 ? hours : '0' + hours
+  minutes = minutes > 9 ? minutes : '0' + minutes
+  seconds = seconds > 9 ? seconds : '0' + seconds
+  return `${days > 0 ? `${days}:` : ``}${(hours || days) > 0 ? `${hours}:` : ``}${minutes}:${seconds}`
 }
 
 function isOnlineInDB(twitch_id) {
